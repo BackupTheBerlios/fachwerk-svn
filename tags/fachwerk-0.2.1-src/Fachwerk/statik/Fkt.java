7dcd74e2c8500d238a5f5fb6eeb6d5aedf77b0ae
qwert2003 <qwert2003@8786838c-82d9-0310-b402-c4e64ea8c326>
2006-09-24 19:44:57 +0000

    Tagging the release fachwerk 0.2.1 (source)

---
+++ tags/fachwerk-0.2.1-src/Fachwerk/statik/Fkt.java
@@ -0,0 +1,196 @@
+/*
+ * Fkt.java
+ *
+ * Created on 26. Dezember 2002, 16:29
+ */
+
+package Fachwerk.statik;
+
+/** 
+ * Copyright (c) 2003 - 2006 A.Vontobel <qwert2003@users.berlios.de>
+ *                                      <qwert2003@users.sourceforge.net>
+ * 
+ * Dieses Programm ist freie Software. Sie können es unter den Bedingungen
+ * der GNU General Public License, Version 2, wie von der Free Software
+ * Foundation herausgegeben, weitergeben und/oder modifizieren.
+ *
+ * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, dass es
+ * Ihnen von Nutzen sein wird, aber OHNE JEDE GEWÄHRLEISTUNG - sogar ohne
+ * die implizite Gewährleistung der MARKTREIFE oder der EIGNUNG FüR EINEN
+ * BESTIMMTEN ZWECK.  Details finden Sie in der GNU General Public License.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen  mit
+ * diesem Programm erhalten haben. Falls nicht, schreiben Sie an die
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+public final class Fkt {
+    
+    /** Creates a new instance of Funktionen */
+    public Fkt() {
+    }
+    
+    public static double holZahl(java.lang.String str) {
+        Double a;
+        
+        try {
+            a = Double.valueOf  ( str );
+            return  a.doubleValue ();
+        }
+        catch (NumberFormatException e)
+        {
+            return 0.0; // ev besser: return Double.NaN;
+        }
+    }
+    
+    public static int holInt(java.lang.String str) {
+        int a;
+        
+        try {
+            a = Integer.parseInt( str );
+            return  a;
+        }
+        catch (NumberFormatException e)
+        {
+            return -1;  // ev besser Fehler weiterleiten
+        }
+    }
+    
+    public static double fix(double zahl, int anzStellen) {
+        double erweitern = Math.pow(10d, anzStellen);
+        return Math.round(erweitern * zahl) / erweitern;
+    }
+    
+    public static double max(double[] liste) {
+        double maximal = liste[0];
+        for (int i=0 ; i < liste.length; i++) {
+            if (liste[i] > maximal) maximal = liste[i];
+        }
+        return maximal;
+    }
+    
+    public static double max(double[][] liste) {
+        double maximal = liste[0][0];
+        for (int i=0 ; i < liste.length; i++) {
+            for (int j = 0; j < liste[i].length; j++) {
+                if (liste[i][j] > maximal) maximal = liste[i][j];
+            }
+        }
+        return maximal;
+    }
+    
+    public static double min(double[] liste) {
+        double minimal = liste[0];
+        for (int i=0 ; i < liste.length; i++) {
+            if (liste[i] < minimal) minimal = liste[i];
+        }
+        return minimal;
+    }
+    
+    /**
+     * löst das 2x2 GLS
+     *                    ax1 * s1 + ax2 * s2 = bx
+     *                    az1 * s1 + az2 * s2 = bz
+     * nach [s1,s2] auf.
+     *
+     * Vor Gebrauch nötigenfalls überprüfen, dass Determinante != 0, siehe Fkt.det2x2(...)
+     */
+    public static double[] GLS2x2(double ax1, double ax2, double bx, double az1, double az2, double bz) {
+        if (det2x2(ax1, ax2 ,az1, az2) == 0d) {
+            System.err.println("Warnung: Determinante = 0, eventuell falsche Resultate!");
+        }
+        //assert det2x2(ax1, ax2 ,az1, az2) != 0: "Determinante = 0";
+        double[] s = new double[2];
+        if (Math.abs(ax1) > Math.abs(az1)) {
+            s[1] = (bz - bx* az1/ax1) / (az2 - ax2* az1/ax1);
+            s[0] = (bx - ax2 * s[1]) / ax1;
+            return s;
+        }
+        else {
+            s[1] = (bx - bz* ax1/az1) / (ax2 - az2* ax1/az1);
+            s[0] = (bz - az2 * s[1]) / az1;
+            return s;
+        }
+    }
+    
+    public static double det2x2(double ax1, double ax2, double az1, double az2) {
+        return ax1 * az2 - ax2 * az1;
+    }
+    
+    /** Normiert einen Vektor, der ursprüngliche Vektor bleibt unverändert
+     @returns normierter Vektor */
+    public static double[] normiere(double[] vektor) {
+        assert vektor.length > 0;
+        double nenner = 0;
+        for (int i = 0; i < vektor.length; i++) {
+            nenner += vektor[i]*vektor[i];
+        }
+        nenner = Math.sqrt(nenner);
+        
+        double[] normiert = new double[vektor.length];
+        for (int i = 0; i < vektor.length; i++) {
+            normiert[i] = vektor[i] / nenner;
+        }
+        return normiert;
+    }
+    
+    
+    /** Gibt einen Zahlenwert als formatierten String zurück.
+     */
+    public static String nf(int zahl, int minimaleAnzahlStellen) {
+        if (minimaleAnzahlStellen < 1) {
+            System.err.print("Formatierungsfehler: minimale Anzahlstellen: ");
+            System.err.println(minimaleAnzahlStellen + " < 1");
+        }
+        StringBuffer ausg = new StringBuffer();
+        ausg.append(zahl);
+        while (ausg.length() < minimaleAnzahlStellen) {
+            ausg.insert(0, " ");
+        }
+        return ausg.toString();
+    }
+    
+    /** Gibt einen Zahlenwert als formatierten String zurück.
+     */
+    public static String nf(double zahl, int AnzNachkommaStellen) {
+	if (Double.isNaN(zahl)) return "NaN";
+	if (Double.isInfinite(zahl)) {
+            if (zahl == Double.NEGATIVE_INFINITY) return "-oo";
+            else return "+oo";
+	}
+        StringBuffer ausg = new StringBuffer();
+        if (AnzNachkommaStellen < 0) {
+            System.err.print("Formatierungsfehler: Anzahl Nachkommastellen: ");
+            System.err.println(AnzNachkommaStellen + " < 0");
+            ausg.append(zahl);
+            return ausg.toString();
+        }
+        double gerundet = fix(zahl, AnzNachkommaStellen);
+        if (AnzNachkommaStellen == 0) ausg.append((int) gerundet);
+        else {
+            ausg.append(gerundet);
+            int vorhNkst = ausg.length() - 1 - ausg.indexOf(".");
+            if (ausg.indexOf("E") < 0) { // nur wenn Zahl nicht in der Form -6.5E-4
+                for (int i = vorhNkst; i < AnzNachkommaStellen; i++) {
+                    ausg.append("0");
+                }
+            }
+        }
+        return ausg.toString();
+    }
+    
+    /** Gibt einen Zahlenwert als formatierten String zurück.
+     */
+    public static String nf(double zahl, int AnzNachkommaStellen, int minAnzStellenVorKomma) {
+        StringBuffer ausg = new StringBuffer();
+        if (AnzNachkommaStellen < 0 || minAnzStellenVorKomma < 0) {
+            System.err.println("Formatierungsfehler");
+            ausg.append(zahl);
+            return ausg.toString();
+        }
+        ausg.append(nf(zahl, AnzNachkommaStellen));
+        while (ausg.length() < (minAnzStellenVorKomma + 1 + AnzNachkommaStellen)) {
+            ausg.insert(0, " ");
+        }
+        return ausg.toString();
+    }
+}
---
+++ tags/fachwerk-0.2.1-src/ru/tcl/dxf/entities/Solid.java
@@ -0,0 +1,207 @@
+// Source file: c:/work/projects/libs/tunegov class libraries/tcl dxf/java/classes/ru/tcl/dxf/entities/Solid.java
+
+/*
+TCL DXF, version 1.0 betta
+Copyright (c) 1999 by Basil Tunegov
+
+THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. THE AUTHOR
+SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENCE AS A RESULT OF USING, MODIFYING
+OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+
+*/
+
+package ru.tcl.dxf.entities;
+
+import java.io.Serializable;
+import java.io.IOException;
+import ru.tcl.dxf.DXFDocument;
+import ru.tcl.dxf.DXFTokenizer;
+import ru.tcl.dxf.DXFPoint;
+
+
+/**
+   Класс <code>Solid</code> представляет замкнутые внутри четырех
+   точек поверхности (заполненные четырехугольниками).
+   @author Basil Tunegov
+   @version 1.2
+   @since TCL DXF 1.0
+ */
+public class Solid extends Entity {
+    private static final int FIRST_CORNER_X = 10;
+    private static final int FIRST_CORNER_Y = 20;
+    private static final int FIRST_CORNER_Z = 30;
+    private static final int SECOND_CORNER_X = 11;
+    private static final int SECOND_CORNER_Y = 21;
+    private static final int SECOND_CORNER_Z = 31;
+    private static final int THIRD_CORNER_X = 12;
+    private static final int THIRD_CORNER_Y = 22;
+    private static final int THIRD_CORNER_Z = 32;
+    private static final int FOURTH_CORNER_X = 13;
+    private static final int FOURTH_CORNER_Y = 23;
+    private static final int FOURTH_CORNER_Z = 33;
+    private DXFPoint first_corner = new DXFPoint ();
+    private DXFPoint second_corner = new DXFPoint ();
+    private DXFPoint third_corner = new DXFPoint ();
+    private DXFPoint fourth_corner = new DXFPoint ();
+
+    public Solid ()
+    {
+        super();
+    }
+
+    public Solid (DXFDocument doc)
+    {
+        super(doc);
+    }
+
+    /**
+       Возвращает наименование примитива.
+       @return наименование примитива.
+       @since TCL DXF 1.0
+     */
+    public final String getEntityName() {
+        return "Solid";
+    }
+
+    /**
+       Возвращает координаты первого угла.
+       @return координаты первого угла.
+       @since TCL DXF 1.0
+       @roseuid 37132F600042
+     */
+    public final DXFPoint getFirstCorner() {
+        return first_corner;
+    }
+    
+    /**
+       Возвращает координаты четвертого угла.
+       @return координаты четвертого угла.
+       @since TCL DXF 1.0
+       @roseuid 37132F600043
+     */
+    public final DXFPoint getFourthCorner() {
+        return fourth_corner;
+    }
+    
+    /**
+       Возвращает координаты второго угла.
+       @return координаты второго угла.
+       @since TCL DXF 1.0
+       @roseuid 37132F600044
+     */
+    public final DXFPoint getSecondCorner() {
+        return second_corner;
+    }
+    
+    /**
+       Возвращает координаты третьего угла.
+       @return координаты третьего угла.
+       @since TCL DXF 1.0
+       @roseuid 37132F600045
+     */
+    public final DXFPoint getThirdCorner() {
+        return third_corner;
+    }
+    
+    /**
+       Считывание объекта из DXF файла. Вы не должны явно
+       использовать данный метод.
+       @param tokenizer разборщик DXF файлов
+       @since TCL DXF 1.0
+       @roseuid 37132F600046
+     */
+    public void readDXF(DXFTokenizer tokenizer) throws IOException {
+        boolean end = false;
+
+        while (end == false)
+        {
+            tokenizer.nextToken();
+
+            if (readCommonPropertiesDXF(tokenizer) == true)
+                continue;
+
+            switch ( tokenizer.getCode() )
+            {
+                case 0:
+                {
+                    end = true;
+                    break;
+                }
+                case 9:
+                {
+                    end = true;
+                    break;
+                }
+                case FIRST_CORNER_X:
+                {
+                    first_corner.setX( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case FIRST_CORNER_Y:
+                {
+                    first_corner.setY( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case FIRST_CORNER_Z:
+                {
+                    first_corner.setZ( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case SECOND_CORNER_X:
+                {
+                    second_corner.setX( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case SECOND_CORNER_Y:
+                {
+                    second_corner.setY( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case SECOND_CORNER_Z:
+                {
+                    second_corner.setZ( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case THIRD_CORNER_X:
+                {
+                    third_corner.setX( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case THIRD_CORNER_Y:
+                {
+                    third_corner.setY( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case THIRD_CORNER_Z:
+                {
+                    third_corner.setZ( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case FOURTH_CORNER_X:
+                {
+                    fourth_corner.setX( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case FOURTH_CORNER_Y:
+                {
+                    fourth_corner.setY( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                case FOURTH_CORNER_Z:
+                {
+                    fourth_corner.setZ( tokenizer.getCommandAsDouble() );
+                    break;
+                }
+                default:
+                {
+                    tokenizer.badInstruction();
+                    break;
+                }
+            }
+        }
+
+        tokenizer.pushBack();
+    }
+}
